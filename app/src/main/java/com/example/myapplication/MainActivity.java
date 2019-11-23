614f0653d9aca3bfa8754bef049f6421e7a8fb22
KKera <kingkera@hotmail.com>
2019-11-24 02:22:52 +0800

    add sign in modual

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.myapplication.ui.login.LoginActivity;
 
 import java.util.Random;
 
@@ -53,6 +58,25 @@ public class MainActivity extends AppCompatActivity {
                 CustomToast.showToast(MainActivity.this,"别点了，安全的很",Toast.LENGTH_SHORT);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu main_activity_menu){
+        getMenuInflater().inflate(R.menu.main_activity_menu,main_activity_menu);
+        return true;
+    }
+
+    @Override
+    public  boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.activity_1:
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            default:
+                CustomToast.showToast(MainActivity.this,"click", Toast.LENGTH_SHORT);
+                break;
+        }
+        return true;
     }
 
 
